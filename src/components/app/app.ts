8933a6f5b493059e63c35ec675d990683f370d53
IrinaPeshko <privet-kira1999@mail.ru>
2023-08-11 16:18:36 +0800

    feat: create main block

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,15 +1,20 @@
 import FooterView from "../footer/footer";
 import HeaderView from "../header/header";
+import MainView from "../main/main";
 
 export default class App {
   public static createView(): void {
     const footerView = new FooterView();
     const headerView = new HeaderView();
+    const mainView = new MainView();
+
     const footerElement = footerView.getHTMLElement();
     const headerElement = headerView.getHTMLElement();
+    const mainElement = mainView.getHTMLElement();
 
-    if (footerElement && headerElement) {
+    if (footerElement && headerElement && mainElement) {
       document.body.append(headerElement);
+      document.body.append(mainElement);
       document.body.append(footerElement);
     } else {
       throw new Error("Element is null.");
