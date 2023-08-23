529385f306ea9e2aaffcda0676d0c28840157304
idiamondcat <shock_harrison@yahoo.com>
2023-08-23 08:54:46 +0700

    fix: fix validation 2023-08-23 08:54:46

--- src/components/utils/validation.ts
+++ src/components/utils/validation.ts
@@ -9,12 +9,9 @@ export default class Validate {
   }
 
   public validateText(): void {
-    const reg = /^[А-яЁёa-zA-Z]{1,}$/;
-    if (this.errorField) {
+    const reg = /^(?! )(?!.* $)(?!(?:.* ){2})[ёЁA-zА-я -]+$/;
       if (!this.target.value || this.target.value === "") {
-        this.errorField.textContent = "This is a required field.";
-        this.target.classList.remove("valid");
-        this.target.classList.add("invalid");
+        this.error("This is a required field.");
       }
       if (this.target.dataset.type === "code") {
         let country: HTMLElement | null;
@@ -24,13 +21,9 @@ export default class Validate {
           if (country) {
             countryVal = (country as HTMLSelectElement).value;
             if (!postcodeValidator(this.target.value, countryVal)) {
-              this.errorField.textContent = "Incorrect postcode.";
-              this.target.classList.remove("valid");
-              this.target.classList.add("invalid");
+              this.error("Incorrect postcode.");
             } else {
-              this.errorField.textContent = "";
-              this.target.classList.remove("invalid");
-              this.target.classList.add("valid");
+              this.valid("Correct postcode!");
             }
           }
         } else if (this.target.id === "shipping_postal_code") {
@@ -38,232 +31,103 @@ export default class Validate {
           if (country) {
             countryVal = (country as HTMLSelectElement).value;
             if (!postcodeValidator(this.target.value, countryVal)) {
-              this.errorField.textContent = "Incorrect postcode.";
-              this.target.classList.remove("valid");
-              this.target.classList.add("invalid");
+              this.error("Incorrect postcode.");
             } else {
-              this.errorField.textContent = "";
-              this.target.classList.remove("invalid");
-              this.target.classList.add("valid");
+              this.valid("Correct postcode!");
             }
           }
         }
-      } else if (this.target.dataset.type === "names") {
+      } else if (
+        this.target.dataset.type === "names" ||
+        this.target.dataset.type === "city"
+      ) {
         if (!reg.test(this.target.value)) {
-          this.errorField.textContent = "Only letters allowed.";
-          this.target.classList.remove("valid");
-          this.target.classList.add("invalid");
+          if (/^\s/.test(this.target.value)) this.target.value = "";
+          this.error(
+            "Only letters, '-' and one space for double names allowed. Spaces at the beginning or end of a line are not allowed.",
+          );
         } else {
-          this.errorField.textContent = "";
-          this.target.classList.remove("invalid");
-          this.target.classList.add("valid");
-        }
-      } else if (this.target.dataset.type === "city") {
-        if (!reg.test(this.target.value)) {
-          this.errorField.textContent = "Must be at least 1 letter.";
-          this.target.classList.remove("valid");
-          this.target.classList.add("invalid");
-        } else {
-          this.errorField.textContent = "";
-          this.target.classList.remove("invalid");
-          this.target.classList.add("valid");
+          this.target.value = this.target.value.replace(
+            /(-| |^)[а-яёa-z]/g,
+            (firstLetter) => firstLetter.toUpperCase(),
+          );
+          this.valid("Correct!");
         }
       } else if (this.target.dataset.type === "street") {
-        const street = /^.{1,}$/;
+        const street = /^(?! )(?!.* $)((?!\s{2}).)+$/;
         if (!street.test(this.target.value)) {
-          this.errorField.textContent = "Must be at least 1 character.";
-          this.target.classList.remove("valid");
-          this.target.classList.add("invalid");
+          if (/^\s/.test(this.target.value)) this.target.value = "";
+          this.error(
+            "Must be at least 1 character. Spaces at the beginning or end of a line or more than one space between characters are not allowed.",
+          );
         } else {
-          this.errorField.textContent = "";
-          this.target.classList.remove("invalid");
-          this.target.classList.add("valid");
+          this.valid("Correct street!");
         }
       }
-    }
   }
 
   public validateEmail(): void {
     const emailReg =
       /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
-    if (this.errorField) {
       if (!this.target.value || this.target.value === "") {
-        this.errorField.textContent = "This is a required field.";
-        (this.errorField as HTMLSpanElement).classList.remove(
-          "form__message--short",
-        );
-        (this.errorField as HTMLSpanElement).classList.remove(
-          "form__message--strong",
-        );
-        (this.errorField as HTMLSpanElement).classList.add(
-          "form__message--empty",
-        );
-        this.target.classList.remove("valid");
-        this.target.classList.add("invalid");
+        this.error("This is a required field.");
       }
       if (!emailReg.test(this.target.value)) {
-        this.errorField.textContent =
-          "Please enter a valid email address, e.g., user@example.com, and ensure it contains no leading or trailing whitespace, includes a domain name, and has an '@' symbol separating the local part and domain name.";
-        (this.errorField as HTMLSpanElement).classList.remove(
-          "form__message--empty",
+        this.error(
+          "Please enter a valid email address, e.g., user@example.com, and ensure it contains no leading or trailing whitespace, includes a domain name, and has an '@' symbol separating the local part and domain name.",
         );
-        (this.errorField as HTMLSpanElement).classList.remove(
-          "form__message--strong",
-        );
-        (this.errorField as HTMLSpanElement).classList.add(
-          "form__message--short",
-        );
-        this.target.classList.remove("valid");
-        this.target.classList.add("invalid");
       } else {
         this.target.value = this.target.value.trim();
-        this.errorField.textContent = "Correct email";
-        (this.errorField as HTMLSpanElement).classList.remove(
-          "form__message--empty",
-        );
-        (this.errorField as HTMLSpanElement).classList.remove(
-          "form__message--short",
-        );
-        (this.errorField as HTMLSpanElement).classList.add(
-          "form__message--strong",
-        );
-        this.target.classList.remove("invalid");
-        this.target.classList.add("valid");
+        this.valid("Correct email!");
       }
-    }
   }
 
   public validatePassword(): void {
     const passwordReg =
-      /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/;
-    if (this.errorField) {
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/;
       if (this.target.id !== "confirm-password") {
+        if (/^\s/.test(this.target.value)) this.target.value = "";
+        if (/\s+$/.test(this.target.value))
+          this.target.value = this.target.value.trimEnd();
         if (!passwordReg.test(this.target.value)) {
-          this.errorField.textContent =
-            "Your password must contain at least 8 characters, at least one uppercase and lowercase letter, digit, and special character (such as !, @, #, $) and must not start or end with a whitespace character.";
-          (this.errorField as HTMLSpanElement).classList.remove(
-            "form__message--empty",
+          this.error(
+            "Your password must contain at least 8 characters, at least one uppercase and lowercase letter, digit, and special character (such as !, @, #, $) and must not start or end with a whitespace character.",
           );
-          (this.errorField as HTMLSpanElement).classList.remove(
-            "form__message--strong",
-          );
-          (this.errorField as HTMLSpanElement).classList.add(
-            "form__message--short",
-          );
-          this.target.classList.remove("valid");
-          this.target.classList.add("invalid");
         } else if (this.target.value.length < 8) {
-          this.errorField.textContent =
-            "Password must be at least 8 characters long";
-          (this.errorField as HTMLSpanElement).classList.remove(
-            "form__message--empty",
-          );
-          (this.errorField as HTMLSpanElement).classList.remove(
-            "form__message--strong",
-          );
-          (this.errorField as HTMLSpanElement).classList.add(
-            "form__message--short",
-          );
-          this.target.classList.remove("valid");
-          this.target.classList.add("invalid");
+          this.error("Password must be at least 8 characters long");
         } else {
-          this.errorField.textContent = "Strong Password!";
-          (this.errorField as HTMLSpanElement).classList.remove(
-            "form__message--empty",
-          );
-          (this.errorField as HTMLSpanElement).classList.remove(
-            "form__message--short",
-          );
-          (this.errorField as HTMLSpanElement).classList.add(
-            "form__message--strong",
-          );
-          this.target.classList.remove("invalid");
-          this.target.classList.add("valid");
+          this.valid("Strong Password!");
         }
       } else {
         const passwordField: HTMLInputElement | null =
           document.querySelector("#password");
-        const passwordValue: string | undefined = passwordField?.value;
-        if (passwordValue && passwordValue !== "") {
-          if (this.target.value === passwordValue) {
-            this.errorField.textContent = "Password confirm!";
-            (this.errorField as HTMLSpanElement).classList.remove(
-              "form__message--empty",
-            );
-            (this.errorField as HTMLSpanElement).classList.remove(
-              "form__message--short",
-            );
-            (this.errorField as HTMLSpanElement).classList.add(
-              "form__message--strong",
-            );
-            this.target.classList.remove("invalid");
-            this.target.classList.add("valid");
-          } else {
-            this.errorField.textContent = "This password is different";
-            (this.errorField as HTMLSpanElement).classList.remove(
-              "form__message--empty",
-            );
-            (this.errorField as HTMLSpanElement).classList.remove(
-              "form__message--strong",
-            );
-            (this.errorField as HTMLSpanElement).classList.add(
-              "form__message--short",
-            );
-            this.target.classList.add("invalid");
+        if (passwordField && passwordField.classList.contains("valid")) {
+          const passwordValue: string | undefined = passwordField.value;
+          if (passwordValue && passwordValue !== "") {
+            if (this.target.value === passwordValue) {
+              this.valid("Password confirm!");
+            } else {
+              this.error("This password is different.");
+            }
           }
+        } else {
+          this.error("This password is invalid.");
         }
       }
-    }
   }
 
   public validateAge(): void {
     const currDate: number = Date.now();
     const userBirth: number = new Date(this.target.value).getTime();
     const difference: number = currDate - userBirth;
-    if (this.errorField) {
-      if (difference < 0) {
-        this.errorField.textContent = "Incorrect date.";
-        (this.errorField as HTMLSpanElement).classList.remove(
-          "form__message--empty",
-        );
-        (this.errorField as HTMLSpanElement).classList.remove(
-          "form__message--strong",
-        );
-        (this.errorField as HTMLSpanElement).classList.add(
-          "form__message--short",
-        );
-        this.target.classList.remove("valid");
-        this.target.classList.add("invalid");
+    if (difference < 0) {
+      this.error("Incorrect date.");
+    } else {
+      const userAge: number = new Date(difference).getFullYear() - 1970;
+      if (userAge < 13) {
+        this.error("Users under the age of 13 are not allowed to register.");
       } else {
-        const userAge: number = new Date(difference).getFullYear() - 1970;
-        if (userAge < 13) {
-          this.errorField.textContent =
-            "Users under the age of 13 are not allowed to register.";
-          (this.errorField as HTMLSpanElement).classList.remove(
-            "form__message--empty",
-          );
-          (this.errorField as HTMLSpanElement).classList.remove(
-            "form__message--strong",
-          );
-          (this.errorField as HTMLSpanElement).classList.add(
-            "form__message--short",
-          );
-          this.target.classList.remove("valid");
-          this.target.classList.add("invalid");
-        } else {
-          this.errorField.textContent = "Allowed age.";
-          (this.errorField as HTMLSpanElement).classList.remove(
-            "form__message--empty",
-          );
-          (this.errorField as HTMLSpanElement).classList.remove(
-            "form__message--short",
-          );
-          (this.errorField as HTMLSpanElement).classList.add(
-            "form__message--strong",
-          );
-          this.target.classList.remove("invalid");
-          this.target.classList.add("valid");
-        }
+        this.valid("Allowed age.");
       }
     }
   }
@@ -279,6 +143,40 @@ export default class Validate {
         this.target.classList.remove("valid");
         this.target.classList.add("invalid");
       }
+    }
+  }
+
+  private error(message: string): void {
+    if (this.errorField) {
+      this.errorField.textContent = message;
+      (this.errorField as HTMLSpanElement).classList.remove(
+        "form__message--empty",
+      );
+      (this.errorField as HTMLSpanElement).classList.remove(
+        "form__message--strong",
+      );
+      (this.errorField as HTMLSpanElement).classList.add(
+        "form__message--short",
+      );
+      this.target.classList.remove("valid");
+      this.target.classList.add("invalid");
+    }
+  }
+
+  private valid(message: string): void {
+    if (this.errorField) {
+      this.errorField.textContent = message;
+      (this.errorField as HTMLSpanElement).classList.remove(
+        "form__message--empty",
+      );
+      (this.errorField as HTMLSpanElement).classList.remove(
+        "form__message--short",
+      );
+      (this.errorField as HTMLSpanElement).classList.add(
+        "form__message--strong",
+      );
+      this.target.classList.remove("invalid");
+      this.target.classList.add("valid");
     }
   }
 }
