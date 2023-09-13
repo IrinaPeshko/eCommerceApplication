33998374923c1683b81b40284f8ba17e416bdf49
idiamondcat <shock_harrison@yahoo.com>
2023-09-13 18:20:19 +0700

    docs: merge commit 2023-09-13 18:20:19

--- src/components/pages/basket/basket.ts
+++ src/components/pages/basket/basket.ts
@@ -1,20 +1,20 @@
-// import Product from "./basketProduct";
-import { getCarts } from "../../../sdk/sdk";
+// // import Product from "./basketProduct";
+// import { getCarts } from "../../../sdk/sdk";
 
-export async function createCartTable(): Promise<void> {
-  // const tableBody: HTMLDivElement | null = document.querySelector(".cart__table-body");
-  try {
-    const cartResp = await getCarts();
-    if (cartResp.statusCode !== 400) {
-      console.log(cartResp.body.results);
-      // const { lineItems } = cartResp.body.results;
-      // cartArr.forEach(elem => {
-      //   console.log(elem);
-      // })
-    } else {
-      throw new Error("Something is wrong");
-    }
-  } catch (err) {
-    console.log(err);
-  }
-}
+// export async function createCartTable(): Promise<void> {
+//   // const tableBody: HTMLDivElement | null = document.querySelector(".cart__table-body");
+//   try {
+//     const cartResp = await getCarts();
+//     if (cartResp.statusCode !== 400) {
+//       console.log(cartResp.body.results);
+//       // const { lineItems } = cartResp.body.results;
+//       // cartArr.forEach(elem => {
+//       //   console.log(elem);
+//       // })
+//     } else {
+//       throw new Error("Something is wrong");
+//     }
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
