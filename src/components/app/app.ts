b2fd860f2d8673ecefc79e0da49e8e05835b5722
idiamondcat <shock_harrison@yahoo.com>
2023-09-20 06:47:06 +0700

    feat: add preloader 2023-09-20 06:47:06

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,19 +1,22 @@
 import FooterView from "../footer/footer";
 import HeaderView from "../header/header";
 import MainView from "../main/main";
+import Preloader from "../preloader/preloader";
 
 export default class App {
   public static createView(): void {
+    const preloaderView = new Preloader();
     const footerView = new FooterView();
     const headerView = new HeaderView();
     const mainView = new MainView();
 
+    const preloaderElem = preloaderView.getHTMLElement();
     const footerElement = footerView.getHTMLElement();
     const headerElement = headerView.getHTMLElement();
     const mainElement = mainView.getHTMLElement();
 
-    if (footerElement && headerElement && mainElement) {
-      document.body.append(headerElement, mainElement, footerElement);
+    if (preloaderElem && footerElement && headerElement && mainElement) {
+      document.body.append(preloaderElem, headerElement, mainElement, footerElement);
     } else {
       throw new Error("Element is null.");
     }
